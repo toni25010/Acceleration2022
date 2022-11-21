406eb0a1d8c50ea4ded9f08b4f4d18bbde5a7052
toni25010 <toni25010@gmail.com>
2022-11-21 22:04:13 +0300

    update_site2

---
+++ script.js
@@ -0,0 +1,24 @@
+window.addEventListener("deviceorientation", handleOrientation, true);
+
+function handleOrientation(event) {
+  var absolute = event.absolute;
+  var alpha    = event.alpha;
+  var beta     = event.beta;
+  var gamma    = event.gamma;
+  
+  $('.absolute').html(absolute);
+  $('.alpha').html(alpha.toFixed(3));
+  $('.beta').html(beta.toFixed(3));
+  $('.gamma').html(gamma.toFixed(3));
+}
+
+window.addEventListener("devicemotion", handleMotion, true);
+
+function handleMotion(event) {
+  var acceleration = event.acceleration;
+  var interval = event.interval;
+  $('.accelerationx').html(acceleration.x.toFixed(3));
+  $('.accelerationy').html(acceleration.y.toFixed(3));
+  $('.accelerationz').html(acceleration.z.toFixed(3));
+  $('.interval').html(interval);
+}
