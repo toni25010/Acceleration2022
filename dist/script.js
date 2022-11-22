deef577e0c2a51831d7cbd3c70b8ead807beb662
Andrey Mazunin <toni25010@gmail.com>
2022-11-22 21:16:31 +0300

    update script.js

--- dist/script.js
+++ dist/script.js
@@ -1,17 +1,23 @@
 function onClick() {
-    // feature detect
-    if (typeof DeviceOrientationEvent.requestPermission === 'function') {
-      DeviceOrientationEvent.requestPermission()
-        .then(permissionState => {
-          if (permissionState === 'granted') {
-            window.addEventListener('deviceorientation', () => {});
-          }
+    if (typeof DeviceMotionEvent.requestPermission === 'function') {
+        // Handle iOS 13+ devices.
+        DeviceMotionEvent.requestPermission()
+            .then(function (state) {
+            if (state === 'granted') {
+                window.addEventListener('devicemotion', handleOrientation);
+            }
+            else {
+                console.error('Request to access the orientation was rejected');
+            }
         })
-        .catch(console.error);
-    } else {
-      // handle regular non iOS 13+ devices
+            .catch(console.error);
     }
-  }
+    else {
+        // Handle regular non iOS 13+ devices.
+        window.addEventListener('devicemotion', handleOrientation);
+    }
+}
+
 
 
 var update = function(id, value) {
